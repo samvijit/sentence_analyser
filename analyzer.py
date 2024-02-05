7ea802afdf7e1bb35076160187c04b22fc31f576
sonaltoppo <88210372+samvijit@users.noreply.github.com>
2024-02-05 18:59:01 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,29 @@
+from string import ascii_uppercase, ascii_lowercase, punctuation, digits
 
+def analyser(str_:str)->dict:
+     l_ = list(str_)
+     low:int = 0
+     upp:int = 0
+     dig:int = 0
+     pun:int = 0
+     for i in l_:
+          s_ = i
+          if s_ in ascii_lowercase:
+               low+=1
+          elif s_ in ascii_uppercase:
+               upp+=1
+          elif s_ in digits:
+               dig+=1
+          elif s_ in punctuation:
+               pun+=1
+     return {
+          "lower":low,
+          "upper":upp,
+          "digit":dig,
+          "punctuation":pun
+     }
+
+if __name__=="__main__":
+	a = str(input("Enter any Text:"))
+	res_ = analyser(a)
+	print(res_)
